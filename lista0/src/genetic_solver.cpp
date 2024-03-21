38e0d35da9cc4114c14f72a8e94249d057314eaa
Enigma717 <mt88x2@gmail.com>
2024-03-21 03:12:06 +0100

    Population generation

--- lista0/src/genetic_solver.cpp
+++ lista0/src/genetic_solver.cpp
@@ -1,4 +1,5 @@
 #include "../include/genetic_solver.h"
+#include "../include/model.h"
 
 GeneticSolver::GeneticSolver(Model& model_ref) : model_ref{model_ref} {}
 
@@ -15,12 +16,20 @@ void GeneticSolver::initialize_population(
 
 void GeneticSolver::random_initialization()
 {
-    for (size_t population_member {0}; population_member < population_size; population_member++) {
+    for (auto& population_member : population) {
+        population_member.solution = model_ref.k_random_solution(100);
+        population_member.objective = model_ref.objective_function(population_member.solution);
     }
 }
 
 void GeneticSolver::neighbour_initialization()
 {
-    for (size_t population_member {0}; population_member < population_size; population_member++) {
+    for (size_t member_position {0}; member_position < population.size(); member_position++) {
+        auto& population_member {population.at(member_position)};
+        const std::uint16_t starting_node_index {
+            static_cast<std::uint16_t>((member_position % model_ref.model_params.dimension) + 1)};
+
+        population_member.solution = model_ref.nearest_neighbour(starting_node_index);
+        population_member.objective = model_ref.objective_function(population_member.solution);
     }
 }
